a31656818bfde41e69a7a77188620dc75f4574db
Gabriela Barbosa <gabrielasbarbosa4@gmail.com>
2022-12-21 12:00:46 -0300

    exercicios funções

---
+++ primeiros_passos_js/aula5.js
@@ -0,0 +1,21 @@
+function meuNome(nome) {
+    console.log('meu nome é ' + nome);
+}
+
+meuNome('gabi');
+
+/* ----------------------------------------------- */
+
+function verificarIdade(idade) {
+    if (idade >= 18) {
+        console.log('você é maior de idade');
+    }
+    else {
+        console.log('você é menor de idade');
+    }
+}
+
+verificarIdade(23);
+verificarIdade(18);
+verificarIdade(17);
+verificarIdade(12);
